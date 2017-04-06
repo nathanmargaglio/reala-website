ba4cf3be863ff7b15323d382fe0331c16f51feed
Nathan Margaglio <nathanmargaglio@gmail.com>
2017-04-05 21:42:54 -0400

    Delete config.py

--- reala/config.py
+++
@@ -1,6 +0,0 @@
-SECRET_KEY = '#r2g=yp+(jy%olbf_#-%8+l3sw15l22d&i%rj!!#j%zh#2l%-z'
-
-DATABASE_NAME = 'reala'
-DATABASE_USER = 'nathanmargaglio'
-DATABASE_PASSWORD = 'margaglio22'
-DATABASE_HOST = 'bflofirst.ccyizcrviele.us-east-1.rds.amazonaws.com'
